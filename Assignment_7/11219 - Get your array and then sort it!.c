6635ea43a4a1bfe7cc388960bb823edd50994f86
duce3790 <duce3790@gmail.com>
2018-12-11 22:20:16 +0800

    2018/12/11/22:20

--- Assignment_7/11219 - Get your array and then sort it!.c
+++ Assignment_7/11219 - Get your array and then sort it!.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "function.h"
 
 int* readInput() {
     int i, N, *seq;
     scanf("%d", &N);
     seq = (int*)malloc(sizeof(int)*(N + 1));
+    //printf("seq %p\n", seq);
     seq[0] = N;
     for(i=1; i<N+1; i++)
         scanf("%d", &seq[i]);
     return seq;
 }
-void my_sort(int *A){
-	int i,j,tmp;
-	for(i=1;i<*A;++i){
-		for(j=1;j<*A-i;++j){
-			if(*(A+j+1)<*(A+j)){
-				tmp=*(A+j+1);
-				*(A+j+1)=*(A+j);
-				*(A+j)=tmp;
-			}
-		}
-	}
+
+void my_sort(int* B) {
+    int i,j,temp;
+    int* arr = *B;
+
+    for(i = 1; i < *arr; i++) {
+        for(j = 1; j <= *(arr) - i; j++) {
+            if(*(arr+j) > *(arr+j+1)){
+                temp = *(arr+j);
+                *(arr+j) = *(arr+j+1);
+                *(arr+j+1) = temp;
+            }
+        }
+    }
 }
 
 int main() {
     int i;
     int *A = readInput();
+    //printf("%p\n", A);
     my_sort((int*)&A);
     printf("%d", A[1]);
     for(i=2;i<A[0]+1;i++)
